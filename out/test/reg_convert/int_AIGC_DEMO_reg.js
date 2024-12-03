53218e7dcdd6876c34d5854d19096f6bd71481b1
刘润喆 <runzhe.liu@aigcic.com>
2024-12-03 10:32:16 +0000

    Update int_AIGC_DEMO_reg.js

--- out/test/reg_convert/int_AIGC_DEMO_reg.js
+++ out/test/reg_convert/int_AIGC_DEMO_reg.js
@@ -39,8 +39,7 @@ try {
                 let packName =''
                 if (debugPattern.test(signalName)) {
                     packName = signalName.replace(debugPattern, '$1'); 
-                    packName = `${packName}_t`
-                    return trimmedLine.replace(dynamicPattern, `${packName.toUpperCase()} reg_${signalName}`);
+                    return trimmedLine.replace(dynamicPattern, `${packName.toUpperCase()}_t reg_${signalName}`);
                 } else {
                     return trimmedLine.replace(dynamicPattern, `${signalName.toUpperCase()}_t reg_${signalName}`);
                 }
